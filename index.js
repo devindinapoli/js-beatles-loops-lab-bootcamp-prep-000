114477beaee57238ee66f1d626a7c8651bf6bba5
Devin DiNapoli <devindinapoli@gmail.com>
2018-02-01 20:56:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function theBeatlesPlay(musicians, instruments){
   const arr = []
   
-  for (let i = 0, 1 = musicians.length; i < 1; i++)
+  for (let i = 0, 1 = musicians.length; i < 1; i++) {
+    arr.push 
+  }
   
 }
